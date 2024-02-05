57733d7dec9f9c9bfadcd98cc4ed7f67b88fc3c1
Suprakon <taresupakon@gmail.com>
2024-02-05 18:26:17 +0700

    add dispatch logic into onClick login button and add userImg into navbar.

--- src/components/NavbarRight.js
+++ src/components/NavbarRight.js
@@ -1,10 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import axios from 'axios';
 
 function NavbarRight() {
     const username = useSelector((state) => state.user.username);
+    const userImg = useSelector(state => state.user.userImg);
+    const dispatch = useDispatch();
+
+    const login = async () => {
+        const user = (await axios.get('https://randomuser.me/api/')).data.results[0];
+        const username = user.login.username;
+        const userImg = user.picture.thumbnail;
+        const actionObject = {
+            type: 'LOGIN',
+            payload: { username, userImg }
+        }
+        dispatch(actionObject);
+    }
+
+    const userloggedIn = username !== '';
     return (
         <div style={{ height: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <span style={{ marginRight: '20px' }}> {username} </span>
+            {userloggedIn ?
+                <>
+                    <span style={{ borderRadius: '50%', backgroundImage: `url(${userImg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '30px', width: '30px', marginRight: '5px' }}></span>
+                    <span style={{ marginRight: '20px' }}> {username} </span>
+                </>
+                :
+                <span onClick={login} style={{ marginRight: '20px', cursor: 'pointer' }}>Click to Login </span>
+            }
         </div>
     )
 }
