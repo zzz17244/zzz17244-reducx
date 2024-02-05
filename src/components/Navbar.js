faf53e0568c03190e22607d1ae2df09b1263feaa
Suprakon <taresupakon@gmail.com>
2024-02-05 16:03:34 +0700

    add box-shadow into Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,8 @@ import NavbarRight from "./NavbarRight"
 function Navbar() {
 
     return (
-        <div style={{ width: '100%', height: '35px', display: 'flex', justifyContent: 'space-between' }}>
+        <div
+            style={{ width: '100%', height: '35px', display: 'flex', justifyContent: 'space-between', boxShadow: 'rgba(33, 35, 38, 0.1) 0px 10px 10px -10px' }}>
             <NavbarLeft />
             <NavbarRight />
         </div>
