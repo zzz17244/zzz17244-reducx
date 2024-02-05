ec44546770cc1c08680c219e1d7e179813f61ea1
Suprakon <taresupakon@gmail.com>
2024-02-05 15:53:47 +0700

    add username.

--- src/components/NavbarRight.js
+++ src/components/NavbarRight.js
@@ -1,8 +1,10 @@
-function NavbarRight() {
+import { useSelector } from "react-redux";
 
+function NavbarRight() {
+    const username = useSelector((state) => state.user.username);
     return (
         <div style={{ height: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
-            <span style={{ marginRight: '20px' }}> username </span>
+            <span style={{ marginRight: '20px' }}> {username} </span>
         </div>
     )
 }
