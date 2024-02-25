0b38b6d8ba221594dc80f2cef7167e9c4f062c7b
Suprakon <taresupakon@gmail.com>
2024-02-26 03:04:54 +0700

    add function dispatch action logout in n

--- src/components/NavbarRight.js
+++ src/components/NavbarRight.js
@@ -17,13 +17,21 @@ function NavbarRight() {
         dispatch(actionObject);
     }
 
+    const logout = () => {
+        const actionType = {
+            type: 'LOGOUT',
+            payload: null
+        }
+        dispatch(actionType);
+    }
+
     const userloggedIn = username !== '';
     return (
         <div style={{ height: '100%', display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
             {userloggedIn ?
                 <>
                     <span style={{ marginRight: '5px' }}> {username} </span>
-                    <span style={{ borderRadius: '50%', backgroundImage: `url(${userImg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '30px', width: '30px', marginRight: '20px' }}></span>
+                    <span onClick={logout} style={{ borderRadius: '50%', backgroundImage: `url(${userImg})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center', height: '30px', width: '30px', marginRight: '20px' }}></span>
                 </>
                 :
                 <span onClick={login} style={{ marginRight: '20px', cursor: 'pointer' }}>Click to Login </span>
